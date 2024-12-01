1ce2442914d8a70df0b154c49b3435455023c518
gavrun <38539655+gavrun@users.noreply.github.com>
2024-12-01 15:13:42 +0000

    Add program file .cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csh_wf_empty
+{
+    internal class Program
+    {
+    }
+}
